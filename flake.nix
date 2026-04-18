c20c3eae23d9cf98d4d04e7ca4cefb555e6714c9
alunity <75143943+alunity@users.noreply.github.com>
2026-04-18 01:42:12 +0100

    separate homemanager without breaking boot (i think)

--- flake.nix
+++ flake.nix
@@ -1,19 +1,13 @@
 {
-  description = "My ephemeral NixOS configuration";
+  description = "Ephemeral NixOS";
 
   inputs = {
-    # 1. The main NixOS package source
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # 2. Disko for partitioning
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-
-    # 3. Impermanence for 'Erase Your Darlings'
     impermanence.url = "github:nix-community/impermanence";
-
     sops-nix.url = "github:Mic92/sops-nix";
-
+    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,25 +19,46 @@
     };
   };
 
-  outputs = { self, nixpkgs, disko, impermanence, sops-nix, lanzaboote, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+  let
+    system = "x86_64-linux";
+    # We define pkgs here to ensure Home Manager uses the exact same nixpkgs as the system
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    # 1. System Configuration (nixos-rebuild switch)
     nixosConfigurations.nixy = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       specialArgs = { inherit inputs; };
       modules = [
-        disko.nixosModules.disko
-        impermanence.nixosModules.impermanence
-        sops-nix.nixosModules.sops 
-        lanzaboote.nixosModules.lanzaboote
-        inputs.home-manager.nixosModules.home-manager
-        {
+        inputs.disko.nixosModules.disko
+        inputs.impermanence.nixosModules.impermanence
+        inputs.sops-nix.nixosModules.sops 
+        inputs.lanzaboote.nixosModules.lanzaboote
+        
+        # Keep this if you want HM to be part of the system build, 
+        # or remove it to keep them 100% isolated.
+        inputs.home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.alunity = import ./home.nix;
           home-manager.extraSpecialArgs = { inherit inputs; };
         }
-        ./configuration.nix          # Your main logic
-        ./disko-config.nix           # The drive layout we discussed
-        ./hardware-configuration.nix # Generated on the machine
+
+        ./configuration.nix
+        ./disko-config.nix
+        ./hardware-configuration.nix
+      ];
+    };
+
+    # 2. Standalone Home Manager (home-manager switch)
+    homeConfigurations."alunity" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = { inherit inputs; };
+      modules = [ 
+        ./home.nix 
       ];
     };
   };
