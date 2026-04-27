d16bf715444f8bbec974aa2f53ae44ba5fbe03c9
alunity <75143943+alunity@users.noreply.github.com>
2026-04-28 00:28:46 +0100

    tweaking power management

--- configuration.nix
+++ configuration.nix
@@ -59,11 +59,11 @@
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
 
       # Helps with Intel's modern "Meteor Lake" efficiency
       CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
+      CPU_BOOST_ON_BAT = 1;
     };
   };
 
